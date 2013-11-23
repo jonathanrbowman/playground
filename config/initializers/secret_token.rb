4608c9a8c9f8f9cc3811bab7a9002d6e8b246778
Jonathan Bowman <jonathanrbowman@me.com>
2013-11-23 16:34:50 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Playground::Application.config.secret_key_base = '3c57117d120624cfc1b201f175ea6106f7ed07ca436e954b15317527d5e76be208c761b17822d9d1d2fef2b90fde8f1fddc0b88649e50345d3f8f79957ddf77a'
