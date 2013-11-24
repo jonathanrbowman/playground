ad278340cfa5dbb8f43ef9156af4be343bff8caf
Jonathan Bowman <jonathanrbowman@me.com>
2013-11-23 19:16:40 -0500

    Added Devise and configured a username table with validation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ Playground::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  
+  config.action_mailer.default_url_options = { :host => 'http://bowmanplayground.herokuapp.com/' }
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Playground::Application.routes.draw do
+  
+  devise_for :users
+  root 'pages#home'
+  
+  get "about" => "pages#about"
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  
-  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
