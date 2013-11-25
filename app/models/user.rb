a094f103e24153fcab1813672dd8cf8083610d0f
Jonathan Bowman <jonathanrbowman@me.com>
2013-11-24 23:51:51 -0500

    Resolved issue with password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  
+    def password_complexity
+   #  if password.present? and not password.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d). /)
+    if password.match(/^(?=.*[^a-zA-Z])(?=.*[a-z])(?=.*[A-Z])\S{8,}$/)
+    else
+      errors.add :password, "must include at least one lowercase letter, one uppercase letter, one number, and be at least 8 characters in length."
+    end
+  end
 
   has_many :projects
 
@@ -9,13 +17,6 @@ class User < ActiveRecord::Base
 
   validates :username, presence: true, uniqueness: true
   validates :user_company, presence: true, uniqueness: true
-  
   validate :password_complexity
   
-  def password_complexity
-    if password.present? and not password.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d). /)
-      errors.add :password, "must include at least one lowercase letter, one uppercase letter, and one number"
-    end
-  end
-
 end
