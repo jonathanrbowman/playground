daa943638dd61dad65318f275d47b2475c6d0e0a
Jonathan Bowman <jonathanrbowman@me.com>
2013-11-24 20:21:32 -0500

    Fixed SSL issue on production environment

--- config/application.rb
+++ config/application.rb
@@ -1,10 +1,14 @@
 require File.expand_path('../boot', __FILE__)
 
+
+
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
+
+
 
 module Playground
   class Application < Rails::Application
@@ -21,6 +25,8 @@ module Playground
     # config.i18n.default_locale = :de
     
     config.assets.initialize_on_precompile = false
+    
+    config.force_ssl = false
     
     
   end
